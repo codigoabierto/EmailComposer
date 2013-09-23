d96625780a6e299198c8dad209d1992ca5ea404f
codigoabierto <jorgeteus@gmail.com>
2013-09-23 09:37:26 -0500

    Testing getThreadPool inside sendEmail method

--- src/android/EmailComposer.java
+++ src/android/EmailComposer.java
@@ -22,16 +22,12 @@ public class EmailComposer extends CordovaPlugin {
         if (action.equals("showEmailComposer")) {
             final JSONObject parameters = args.getJSONObject(0);
             if(parameters != null){
-            	cordova.getThreadPool().execute(new Runnable(){
-            		public void run(){
-		            	try{
-		            		sendEmail(parameters);	
-		            		callbackContext.success("Sent email successfully");
-		            	} catch(Exception e){
-		            		callbackContext.error(e.toString());
-		            	}
-	            	}
-            	});
+            	try{
+            		sendEmail(parameters);	
+            		callbackContext.success("Sent email successfully");
+            	} catch(Exception e){
+            		callbackContext.error(e.toString());
+            	}
             }
             return true;
         }
@@ -40,118 +36,127 @@ public class EmailComposer extends CordovaPlugin {
 
     private void sendEmail(JSONObject parameters) {
 
-		final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND_MULTIPLE);
 
-		//String callback = parameters.getString("callback");
+    	this.cordova.getThreadPool().execute(new Runnable(){
+    		public void run(){
 
-		boolean isHTML = false;
-		try {
-			isHTML = parameters.getBoolean("isHTML");
-		} catch (Exception e) {
-			Log.e("EmailComposer", "Error handling isHTML param: " + e.toString());
-		}
+    			Context context = cordova.getActivity().getApplicationContext();
+				final Intent emailIntent = new Intent(context, android.content.Intent.ACTION_SEND_MULTIPLE);
 
-		if (isHTML) {
-			emailIntent.setType("text/html");
-		} else {
-			emailIntent.setType("text/plain");
-		}
+				//String callback = parameters.getString("callback");
 
-		// setting subject
-		try {
-			String subject = parameters.getString("subject");
-			if (subject != null && subject.length() > 0) {
-				emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
-			}
-		} catch (Exception e) {
-			Log.e("EmailComposer", "Error handling subject param: " + e.toString());
-		}
+				boolean isHTML = false;
+				try {
+					isHTML = parameters.getBoolean("isHTML");
+				} catch (Exception e) {
+					Log.e("EmailComposer", "Error handling isHTML param: " + e.toString());
+				}
 
-		// setting body
-		try {
-			String body = parameters.getString("body");
-			if (body != null && body.length() > 0) {
 				if (isHTML) {
-					emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, Html.fromHtml(body));
+					emailIntent.setType("text/html");
 				} else {
-					emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, body);
+					emailIntent.setType("text/plain");
 				}
-			}
-		} catch (Exception e) {
-			Log.e("EmailComposer", "Error handling body param: " + e.toString());
-		}
 
-		/*
-		// setting TO recipients
-		try {
-			JSONArray toRecipients = parameters.getJSONArray("toRecipients");
-			if (toRecipients != null && toRecipients.length() > 0) {
-				String[] to = new String[toRecipients.length()];
-				for (int i=0; i<toRecipients.length(); i++) {
-					to[i] = toRecipients.getString(i);
-				}
-				emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, to);
-			}
-		} catch (Exception e) {
-			Log.e("EmailComposer", "Error handling toRecipients param: " + e.toString());
-		}
-
-		// setting CC recipients
-		try {
-			JSONArray ccRecipients = parameters.getJSONArray("ccRecipients");
-			if (ccRecipients != null && ccRecipients.length() > 0) {
-				String[] cc = new String[ccRecipients.length()];
-				for (int i=0; i<ccRecipients.length(); i++) {
-					cc[i] = ccRecipients.getString(i);
-				}
-				emailIntent.putExtra(android.content.Intent.EXTRA_CC, cc);
-			}
-		} catch (Exception e) {
-			Log.e("EmailComposer", "Error handling ccRecipients param: " + e.toString());
-		}
-
-		// setting BCC recipients
-		try {
-			JSONArray bccRecipients = parameters.getJSONArray("bccRecipients");
-			if (bccRecipients != null && bccRecipients.length() > 0) {
-				String[] bcc = new String[bccRecipients.length()];
-				for (int i=0; i<bccRecipients.length(); i++) {
-					bcc[i] = bccRecipients.getString(i);
-				}
-				emailIntent.putExtra(android.content.Intent.EXTRA_BCC, bcc);
-			}
-		} catch (Exception e) {
-			Log.e("EmailComposer", "Error handling bccRecipients param: " + e.toString());
-		}
-
-		// setting attachments
-		try {
-			JSONArray attachments = parameters.getJSONArray("attachments");
-			if (attachments != null && attachments.length() > 0) {
-				ArrayList<Uri> uris = new ArrayList<Uri>();
-				//convert from paths to Android friendly Parcelable Uri's
-				for (int i=0; i<attachments.length(); i++) {
-					try {
-						File file = new File(attachments.getString(i));
-						if (file.exists()) {
-							Uri uri = Uri.fromFile(file);
-							uris.add(uri);
-						}
-					} catch (Exception e) {
-						Log.e("EmailComposer", "Error adding an attachment: " + e.toString());
+				// setting subject
+				try {
+					String subject = parameters.getString("subject");
+					if (subject != null && subject.length() > 0) {
+						emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
 					}
+				} catch (Exception e) {
+					Log.e("EmailComposer", "Error handling subject param: " + e.toString());
 				}
-				if (uris.size() > 0) {
-					emailIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
-				}
-			}
-		} catch (Exception e) {
-			Log.e("EmailComposer", "Error handling attachments param: " + e.toString());
-		}
-		*/
 
-		//this.cordova.startActivityForResult(this, emailIntent, 0);
-		this.cordova.getActivity().startActivity(emailIntent);
+				// setting body
+				try {
+					String body = parameters.getString("body");
+					if (body != null && body.length() > 0) {
+						if (isHTML) {
+							emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, Html.fromHtml(body));
+						} else {
+							emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, body);
+						}
+					}
+				} catch (Exception e) {
+					Log.e("EmailComposer", "Error handling body param: " + e.toString());
+				}
+
+				/*
+				// setting TO recipients
+				try {
+					JSONArray toRecipients = parameters.getJSONArray("toRecipients");
+					if (toRecipients != null && toRecipients.length() > 0) {
+						String[] to = new String[toRecipients.length()];
+						for (int i=0; i<toRecipients.length(); i++) {
+							to[i] = toRecipients.getString(i);
+						}
+						emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, to);
+					}
+				} catch (Exception e) {
+					Log.e("EmailComposer", "Error handling toRecipients param: " + e.toString());
+				}
+
+				// setting CC recipients
+				try {
+					JSONArray ccRecipients = parameters.getJSONArray("ccRecipients");
+					if (ccRecipients != null && ccRecipients.length() > 0) {
+						String[] cc = new String[ccRecipients.length()];
+						for (int i=0; i<ccRecipients.length(); i++) {
+							cc[i] = ccRecipients.getString(i);
+						}
+						emailIntent.putExtra(android.content.Intent.EXTRA_CC, cc);
+					}
+				} catch (Exception e) {
+					Log.e("EmailComposer", "Error handling ccRecipients param: " + e.toString());
+				}
+
+				// setting BCC recipients
+				try {
+					JSONArray bccRecipients = parameters.getJSONArray("bccRecipients");
+					if (bccRecipients != null && bccRecipients.length() > 0) {
+						String[] bcc = new String[bccRecipients.length()];
+						for (int i=0; i<bccRecipients.length(); i++) {
+							bcc[i] = bccRecipients.getString(i);
+						}
+						emailIntent.putExtra(android.content.Intent.EXTRA_BCC, bcc);
+					}
+				} catch (Exception e) {
+					Log.e("EmailComposer", "Error handling bccRecipients param: " + e.toString());
+				}
+
+				// setting attachments
+				try {
+					JSONArray attachments = parameters.getJSONArray("attachments");
+					if (attachments != null && attachments.length() > 0) {
+						ArrayList<Uri> uris = new ArrayList<Uri>();
+						//convert from paths to Android friendly Parcelable Uri's
+						for (int i=0; i<attachments.length(); i++) {
+							try {
+								File file = new File(attachments.getString(i));
+								if (file.exists()) {
+									Uri uri = Uri.fromFile(file);
+									uris.add(uri);
+								}
+							} catch (Exception e) {
+								Log.e("EmailComposer", "Error adding an attachment: " + e.toString());
+							}
+						}
+						if (uris.size() > 0) {
+							emailIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+						}
+					}
+				} catch (Exception e) {
+					Log.e("EmailComposer", "Error handling attachments param: " + e.toString());
+				}
+				*/
+
+				//this.cordova.startActivityForResult(this, emailIntent, 0);
+				this.cordova.getActivity().startActivity(emailIntent);
+
+        	}
+    	});
+
 	}
 
 	@Override
